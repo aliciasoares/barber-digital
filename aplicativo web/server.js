10e2b1df3a40a950397c59547858dfc2cb9f81b8
Alicia Oliveira Soares <Alicia Oliveira Soares>
2026-04-30 15:32:57 -0300

    Projeto: Configuração

---
+++ aplicativo web/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+class Server
+{
+
+    app
+    port
+
+    constructor(port)
+    {
+        this.app = express()
+        this.port = port
+    }
+
+    listen()
+    {
+        this.app.listen(this.port, () => {
+            console.log("Servidor Online...")
+        })
+    }
+}
+
+module.exports = new Server()
