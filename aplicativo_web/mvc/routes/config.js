8268bbcec035bb1d61a24a5f92cae0e2e252258b
Aliciasoares <aliciaoliveirasoaress@gmail.com>
2026-05-19 11:00:00 -0300

    Erros da view consertados

--- aplicativo_web/mvc/routes/config.js
+++ aplicativo_web/mvc/routes/config.js
@@ -4,7 +4,7 @@ const AtendimentoController = require("../controllers/AtendimentoController")
 
 const router = Router()
 //rotas do usuario
-router.get("/user/index", (req, res) => UsuarioController.index(req, res))
+router.get("/user/", (req, res) => UsuarioController.index(req, res))
 router.post("/user/create", (req, res) => UsuarioController.usuarioPostAsync(req, res))
 router.get("/user/create", (req, res) => UsuarioController.usuarioCreateView(req, res))
 router.put("/user/edit", (req, res) => UsuarioController.usuarioPutAsync(req, res))
